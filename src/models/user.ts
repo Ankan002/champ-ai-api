8ad47da4ab6c5fdbb698661871abc3aaf84fb771
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-06-28 00:12:57 +0530

    chore: :wrench: add user schema

---
+++ src/models/user.ts
@@ -0,0 +1,79 @@
+import { Schema, model } from "mongoose";
+import { UserType } from "typings/models";
+
+enum ActivePicture {
+	ProfilePicture = "profile_picture",
+	Avatar = "avatar",
+}
+
+const userSchema = new Schema<UserType>(
+	{
+		email: {
+			type: String,
+			required: true,
+			min: 1,
+			max: 60,
+			trim: true,
+			unique: true,
+		},
+		name: {
+			type: String,
+			required: true,
+			min: 3,
+			max: 70,
+			trim: true,
+		},
+		provider_id: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+		},
+		profile_pic: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		avatar: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		premium_till: {
+			type: String,
+			default: null,
+			trim: true,
+		},
+		premium_plan: {
+			type: Schema.Types.ObjectId,
+			ref: "Plan",
+			default: null,
+		},
+		is_active: {
+			type: Boolean,
+			default: true,
+		},
+		active_picture: {
+			type: String,
+			enum: ActivePicture,
+			default: ActivePicture.Avatar,
+		},
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			transform: (_, returningDoc) => {
+				returningDoc["id"] = returningDoc["_id"];
+				delete returningDoc["_id"];
+			},
+		},
+	}
+);
+
+export const User = model<UserType>("User", userSchema);
---
+++ src/typings/models/user.ts
@@ -0,0 +1,19 @@
+import { ObjectId } from "mongoose";
+
+export interface UserType {
+	_id: ObjectId;
+	email: string;
+	username: string;
+	name: string;
+	provider_id: string;
+	profile_pic: string;
+	avatar: string;
+	premium_till: string;
+	premium_plan: ObjectId;
+	is_active: boolean;
+	active_picture: "profile_pic" | "avatar";
+	id: string;
+	createdAt: string;
+	updatedAt: string;
+	__v: number;
+}
