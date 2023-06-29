f85bee8433c0f9f62a58a07c5400358198f0491a
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-06-29 11:23:51 +0530

    feat: :sparkles: add is unauthenticated middleware

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -1,6 +1,7 @@
 import { login } from "controllers/auth";
 import { Router } from "express";
+import { isUnauthenticated } from "middlewares/auth";
 
 export const authRouter = Router();
 
-authRouter.route("/login").post(login);
+authRouter.route("/login").post(isUnauthenticated, login);
